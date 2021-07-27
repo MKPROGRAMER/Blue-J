a19c818c0c04e493c3647cd37419b013db036d31
MKPROGRAMER <83109689+MKPROGRAMER@users.noreply.github.com>
2021-07-27 23:46:57 +0530

    Create random.js

---
+++ Programs/random.js
@@ -0,0 +1,10 @@
+/* Program for a random number */
+public class mathsclass
+{
+  public static void main (string args[])
+  {
+    double num;
+    num = Math.random();
+    System.out.println("Randome number is :-  " +num );
+  }
+}
